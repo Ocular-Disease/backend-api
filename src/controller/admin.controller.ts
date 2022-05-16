eba541c2c9035c0873f8447fd8466a731afc6d41
Marodioss <96569588+Marodioss@users.noreply.github.com>
2022-05-16 15:40:06 +0100

    Update admin.controller.ts

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -87,7 +87,7 @@ class AdminController {
 		res.cookie('refresh_token', refresh_token.id, {
 			expires: moment().add(90, 'day').toDate(),
 			httpOnly: true,
-			sameSite: 'lax',
+			sameSite: 'none',
 		});
 
 		res.status(200).json(user);
