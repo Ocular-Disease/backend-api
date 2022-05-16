8a51887cb1c491c41233c34a34a86f1ff5a30f36
darklight147 <39389636+darklight147@users.noreply.github.com>
2022-05-16 21:03:05 +0100

    fix: graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -29,25 +29,24 @@ async function main() {
 			console.log('SIGTERM signal received.');
 			server.close(() => {
 				console.log('Process terminated.');
-				process.exit(0);
+				PostgresDataSource.destroy().then(res => {
+					console.log('Database destroyed');
+					process.exit(0);
+				})
 			});
 
-			PostgresDataSource.destroy().then(res => {
-				console.log('Database destroyed');
-				process.exit(0);
-			})
 		});
 
 		process.on('SIGINT', () => {
 			console.log('SIGINT signal received.');
 			server.close(() => {
 				console.log('Process terminated.');
+				PostgresDataSource.destroy().then(res => {
+					console.log('Database destroyed');
+					process.exit(0);
+				})
 			});
 
-			PostgresDataSource.destroy().then(res => {
-				console.log('Database destroyed');
-				process.exit(0);
-			})
 		});
 
 	} catch (error: any) {
