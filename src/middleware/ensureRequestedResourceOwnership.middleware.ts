526007124ffcd78c3b9fcdd8255c5fd8398b6877
{{ github.actor }} <belkamelmohamed@gmail.com>
2022-05-17 15:50:04 +0100

    fix: skip ensure ownership if admin

--- src/middleware/ensureRequestedResourceOwnership.middleware.ts
+++ src/middleware/ensureRequestedResourceOwnership.middleware.ts
@@ -1,7 +1,12 @@
 import {Request, Response, NextFunction} from 'express';
 import { UnauthorizedError } from '../error/UnauthorizedError.error';
+import { Role } from '../types/role.enum';
 
 export const ensureRequestedResourceOwnership = (req: Request, res: Response, next: NextFunction) => {
+    if (req.currentUser?.role === Role.ADMIN) {
+        return next();
+    }
+    
     if (req.params.id) {
         if (req.params.id !== req.currentUser?.userId) {
             throw new UnauthorizedError('You are not authorized to access this resource');
