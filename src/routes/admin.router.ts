e4b4c57227bbf0f1d04740abae224ac162ebd2ff
{{ github.actor }} <belkamelmohamed@gmail.com>
2022-05-17 15:26:35 +0100

    chores: fix routes and plug secretaire to to server

--- src/routes/admin.router.ts
+++ src/routes/admin.router.ts
@@ -40,12 +40,11 @@ class AdminRouter {
 			adminController.create
 		);
 		this.router.post('/login', adminController.login);
-		this.router.get('/tokens', ensureAuthenticated, ensureAccessLevel(Role.ADMIN), adminController.getTokens);
 		this.router.get('/logout', adminController.logout);
 		this.router.get(
 			'/upload',
 			ensureAuthenticated,
-			ensureAccessLevel(Role.ADMIN),
+			ensureAccessLevel(Role.MEDECIN),
 			adminController.getPresignedUrl
 		);
 		this.router.get(
--- src/routes/shared.router.ts
+++ src/routes/shared.router.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.middleware";
+import adminService from "../service/admin.service";
+import medecinService from "../service/medecin.service";
+import secretaireService from "../service/secretaire.service";
+import { Role } from "../types/role.enum";
 
 class SharedRouter {
     public router: express.Router;
@@ -8,13 +13,36 @@ class SharedRouter {
         this.config();
     }
     config() {
-        this.router.get("/logout", (req, res) => {
+        this.router.get("/logout", ensureAuthenticated, (req, res) => {
             req.session.access_token = undefined;
-
 
             res.status(200).json({
                 message: "Logged out"
             });
+        });
+
+        this.router.get('/me', ensureAuthenticated, (req, res) => {
+            res.status(200).json(req.currentUser);
+        });
+
+        this.router.get('/me/details', ensureAuthenticated, (req, res) => {
+            const {userId, role} = req.currentUser!;
+
+            if (role === Role.ADMIN) {
+                adminService.getById(userId).then(admin => {
+                    res.status(200).json({...admin, password: undefined});
+                });
+            } else if (role === Role.MEDECIN) {
+                medecinService.getById(userId).then(medecin => {
+                    res.status(200).json({...medecin, password: undefined});
+                });
+            } else if (role === Role.SECRETAIRE) {
+                secretaireService.getById(userId).then(secretaire => {
+                    res.status(200).json({...secretaire, password: undefined});
+                });
+            } else {
+                res.status(400).json({message: "Invalid role"});
+            }
         });
     }
 }
