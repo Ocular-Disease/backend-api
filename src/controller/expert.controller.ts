e3fe00e81e89ab6dadaba5d0c9bfc9aeb34aa274
darklight147 <39389636+darklight147@users.noreply.github.com>
2022-05-23 23:04:47 +0100

    feat: login

--- src/controller/expert.controller.ts
+++ src/controller/expert.controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
+import moment from 'moment';
+import { config } from '../config/env.config';
 import { BadRequestException } from '../error/BadRequestException.error';
 import { Expert } from '../model/expert';
 import expertService from '../service/expert.service';
+import jwtService from '../service/jwt.service';
 import passwordService from '../service/password.service';
+import tokenService from '../service/token.service';
+import { Role } from '../types/role.enum';
 
 class ExpertController {
     public async allExperts(req: Request, res: Response) {
@@ -75,6 +80,46 @@ class ExpertController {
 
     }
 
+    public async login(req: Request, res: Response) {
+        const { login, password } = req.body;
+
+        const user = await expertService.getExpertByEmail(login);
+
+        if (!user) {
+            throw new BadRequestException('Invalid credentials');
+        }
+
+        const isPasswordValid = await passwordService.comparePassword(
+            password,
+            user.password
+        );
+
+        if (!isPasswordValid) {
+            throw new BadRequestException('Invalid credentials');
+        }
+
+        req.session.access_token = jwtService.sign({
+            userId: user.id,
+            role: Role.ADMIN,
+        });
+
+        req.sessionOptions.expires = moment().add(1, 'day').toDate();
+        const refreshToken = jwtService.signRefreshToken({
+            userId: user.id,
+            role: Role.ADMIN,
+        });
+
+        const refresh_token = await tokenService.createToken(user, refreshToken);
+
+        res.cookie('refresh_token', refresh_token.id, {
+            expires: moment().add(90, 'day').toDate(),
+            httpOnly: true,
+            sameSite: 'none',
+            secure: config.NODE_ENV === 'production',
+        });
+
+        res.status(200).json({ ...user, password: undefined });
+    }
 }
 
 export default new ExpertController();
